c34f40e17f0a869eef13295a569890b232e1198f
Usman Rajab <usman.rajab@tink.se>
2020-09-22 23:06:47 +0200

    add ts type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export const log = (msg: string) => console.log(`${msg}`);
+type BrowserResult = { name: string, version: string };
+type DeviceResult = { model: string, type: string, vendor: string };
+type EngineResult = { name: string, version: string };
+type OSResult = { name: string, version: string };
+type CpuResult = { architecture: string };
+
+export function UaHog(ua: string): BrowserResult | DeviceResult | EngineResult | OSResult | CpuResult  | null {
+
+}
