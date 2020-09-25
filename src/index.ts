444b0d8acbe45040aa0445634c9a41061d23b212
Usman Rajab <usman.rajab@tink.se>
2020-09-25 23:41:03 +0200

    refactor and add methods for getting device/platform

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,24 @@ type BrowserResult = { name: string, version: string };
 type DeviceResult = { model: string, type: string, vendor: string };
 type EngineResult = { name: string, version: string };
 type OSResult = { name: string, version: string };
-type CpuResult = { architecture: string };
 
-export function UaHog(ua: string): BrowserResult | DeviceResult | EngineResult | OSResult | CpuResult  | null {
+export function parseUserAgent(ua: string): BrowserResult | DeviceResult | EngineResult | OSResult | null {
 
 }
+
+export function getUserAgent(): string {
+  if(typeof navigator === 'object' && 'userAgent' in navigator){
+    return navigator.userAgent;
+  }
+  // Node
+  if(typeof process === "object" && 'version' in process){
+    return `Node.js/${process.version.substr(1)} (${process.platform}; ${process.arch
+      })`;
+  }
+  return "<environment undetectable>";
+}
+
+export function isAppleDevice(ua: string): boolean{}
+export function isAndroidDevice(ua: string): boolean{}
+export function isMobile(ua: string): boolean{}
+export function isDesktop(ua: string): boolean{}
